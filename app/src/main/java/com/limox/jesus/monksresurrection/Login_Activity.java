f13cd01eaf7cb0fe5537d91e652dcf6abd09f6be
JesusLX <jpmx96@gmail.com>
2016-11-09 23:26:31 +0100

    Añadidas activities para logn up y recuperación de cuenta, login mejorado

--- app/src/main/java/com/limox/jesus/monksresurrection/Login_Activity.java
+++ app/src/main/java/com/limox/jesus/monksresurrection/Login_Activity.java
@@ -1,13 +1,41 @@
 package com.limox.jesus.monksresurrection;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
 
+    TextView mTxvSignUp;
+    TextView mTxvFP;
+    AdapterView.OnClickListener mCliclListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mTxvSignUp = (TextView) findViewById(R.id.lgn_txvSignUp);
+        mTxvFP = (TextView) findViewById(R.id.lgn_txvFP);
+
+        mCliclListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.lgn_txvFP:
+                        startActivity(new Intent(Login_Activity.this,HelpLogin_Activity.class));
+                        break;
+                    case R.id.lgn_txvSignUp:
+                        startActivity(new Intent(Login_Activity.this,StartSignUp_Activity.class));
+                        break;
+                }
+            }
+        };
+
+        mTxvFP.setOnClickListener(mCliclListener);
+        mTxvSignUp.setOnClickListener(mCliclListener);
     }
+
+
 }
---
+++ app/src/main/java/com/limox/jesus/monksresurrection/SignUpEmail_Activity.java
@@ -0,0 +1,17 @@
+package com.limox.jesus.monksresurrection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SignUpEmail_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_email);
+    }
+}
