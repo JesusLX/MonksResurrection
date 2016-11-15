c1dfd93e75499457624dd1f233cecd07c7b595c3
JesusLX <jpmx96@gmail.com>
2016-11-15 21:58:54 +0100

    Ya se puede recuperar contraseña

--- app/src/main/java/com/limox/jesus/monksresurrection/HelpLoginPassword_Activity.java
+++ app/src/main/java/com/limox/jesus/monksresurrection/HelpLoginPassword_Activity.java
@@ -1,13 +1,53 @@
 package com.limox.jesus.monksresurrection;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.limox.jesus.monksresurrection.Model.User;
+import com.limox.jesus.monksresurrection.Singleton.Users_Singleton;
 
 public class HelpLoginPassword_Activity extends AppCompatActivity {
+
+    EditText mEdtEmail;
+    Button mBtnNext;
+    String mEmail;
+    View.OnClickListener mListenerOnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_login_password);
+        // Inicializate
+        mEdtEmail = (EditText) findViewById(R.id.hlp_edtEmail);
+        mBtnNext = (Button) findViewById(R.id.hlp_btnSend);
+        createOnClick();
+        mBtnNext.setOnClickListener(mListenerOnClick);
+
+
+    }
+
+    void createOnClick(){
+        mListenerOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the email introducced
+                mEmail = mEdtEmail.getText().toString();
+                User tmpUer;
+                if ((tmpUer = Users_Singleton.getUsers_Singleton().getUserByEmail(mEmail)) != null){
+                    Bundle bundle = new Bundle();
+                    bundle.putString("email",mEmail);
+                    Intent intent = new Intent(HelpLoginPassword_Activity.this,HelpLoginFinal_Activity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }else{
+                    mEdtEmail.setError(getString(R.string.message_error_email_not_registred));
+                }
+
+            }
+        };
     }
 }
--- app/src/main/java/com/limox/jesus/monksresurrection/Singleton/Users_Singleton.java
+++ app/src/main/java/com/limox/jesus/monksresurrection/Singleton/Users_Singleton.java
@@ -19,14 +19,14 @@ public class Users_Singleton {
     private static Users_Singleton mUsers_Singleton;
     List<User> mUsers;
 
-    public static Users_Singleton getUsers_Singleton(){
-        if (mUsers_Singleton == null){
+    public static Users_Singleton getUsers_Singleton() {
+        if (mUsers_Singleton == null) {
             mUsers_Singleton = new Users_Singleton();
         }
         return mUsers_Singleton;
     }
 
-    private Users_Singleton(){
+    private Users_Singleton() {
         mUsers = new ArrayList<User>();
         //TODO Create example users
         addExampleUsers();
@@ -36,61 +36,80 @@ public class Users_Singleton {
     /**
      * Create 1 admin user and 2 users for examples
      */
-    private void addExampleUsers(){
-        if (mUsers != null){
-            mUsers.add(new User(0,0,"Admin1","ejemplo1@gmail.com","123", R.drawable.nino,0));
-            mUsers.add(new User(1,1,"User1","ejemplo2@gmail.com","123", R.drawable.monje,1));
-            mUsers.add(new User(2,2,"User2","ejemplo3@gmail.com","123", R.drawable.icomonge,1));
+    private void addExampleUsers() {
+        if (mUsers != null) {
+            mUsers.add(new User(0, 0, "Admin1", "ejemplo1@gmail.com", "123", R.drawable.nino, 0));
+            mUsers.add(new User(1, 1, "User1", "ejemplo2@gmail.com", "123", R.drawable.monje, 1));
+            mUsers.add(new User(2, 2, "User2", "ejemplo3@gmail.com", "123", R.drawable.icomonge, 1));
         }
     }
-    public List<User> getUsers(){
+
+    public List<User> getUsers() {
         return mUsers;
     }
-    public void setUsers(List<User> users){
+
+    public void setUsers(List<User> users) {
         this.mUsers = users;
     }
-    public boolean addUser(User user){
+
+    public boolean addUser(User user) {
         boolean allFine = false;
-        if (!mUsers.contains(user)){
+        if (!mUsers.contains(user)) {
             mUsers.add(user);
             allFine = true;
         }
         return allFine;
     }
-    public boolean addUser(String userName,String password, String email){
+
+    public boolean addUser(String userName, String password, String email) {
         boolean allFine = false;
-        User user = new User(0,mUsers.size(),userName,email,password,R.drawable.monje,1);
-        if (!mUsers.contains(user)){
+        User user = new User(0, mUsers.size(), userName, email, password, R.drawable.monje, 1);
+        if (!mUsers.contains(user)) {
             mUsers.add(user);
             allFine = true;
         }
         return allFine;
     }
-    public boolean delUser(User user){
+
+    public boolean delUser(User user) {
         boolean allFine = false;
-        if (mUsers.contains(user)){
+        if (mUsers.contains(user)) {
             mUsers.remove(mUsers.indexOf(user));
             allFine = true;
         }
         return allFine;
     }
-    public User getUserById(int idUser){
+
+    public User getUserById(int idUser) {
         User user = null;
         if (this.mUsers.contains(new User(idUser)))
-         user = this.mUsers.get(mUsers.indexOf(new User(idUser)));
+            user = this.mUsers.get(mUsers.indexOf(new User(idUser)));
 
         return user;
     }
-    public void setCurrentUser(User user){
+
+    public User getUserByEmail(String email) {
+        User user = null;
+        for (User tmpUser : mUsers) {
+            if (tmpUser.getEmail().equals(email))
+                return tmpUser;
+        }
+        return user;
+    }
+
+
+    public void setCurrentUser(User user) {
         currentUser = user;
     }
-    public User getCurrentUser(){
+
+    public User getCurrentUser() {
         return currentUser;
     }
-    public User getUser(String userName){
+
+    public User getUser(String userName) {
         //TODO No funciona el equals
         // if (mUsers.contains(new User(userName)))
-        for (User tmpUser: mUsers) {
+        for (User tmpUser : mUsers) {
             if (tmpUser.getNick().equals(userName))
                 return tmpUser;
 
